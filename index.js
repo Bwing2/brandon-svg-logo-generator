aa3b7eebd6667f21278c7193d4f744faaeca60d5
Brandon Wing <brandon.wing245@gmail.com>
2023-10-29 15:31:46 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
+
+const questions = [
+  {
+    type: "input",
+    name: "letters",
+    message: "Please choose up to 3 letters to use for your logo.",
+    validate: (letters) => {
+      if (letters.length > 3) {
+        return "Please use up to 3 letters only.";
+      } else {
+        return !letters ? "Please enter at least 1 letter" : true;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message:
+      "Please enter a color or hexadecimal number for your logo's text color.",
+    validate: (text) => {
+      return !text ? "Please enter a color or hexadecimal number." : true;
+    },
+  },
+  {
+    type: "list",
+    name: "shape",
+    choices: ["Circle", "Triangle", "Square"],
+    message: "Please choose your desired shape.",
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message:
+      "Please enter a color or hexadecimal number for your shape's color.",
+    validate: (shape) => {
+      return !shape ? "Please enter a color or hexadecimal number." : true;
+    },
+  },
+];
+
+function writeToFile(filename, data) {
+  fs.writeFile(filename, data, (err) => {
+    err ? console.log(err) : console.log("Logo generated!");
+  });
+}
+
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const filename = "logo.svg";
+    writeToFile(filename);
+  });
+}
+
+init();
